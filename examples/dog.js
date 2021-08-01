f7054ba868eaed16ca861f23cf0790b6c8c5b560
Vitalij Ryndin <sys@crashmax.ru>
2021-08-01 16:36:18 +0800

    refactor: examples

--- examples/dog.js
+++ examples/dog.js
@@ -1,4 +1,6 @@
-const someRandomAPI = require('node-some-random-api');
-const client = new someRandomAPI.SRAClient();
+const { SRAClient } = require('node-some-random-api');
+const client = new SRAClient();
 
-client.dog().then((response) => console.log(`Look at this cute dog! ${response.image}`));
+client
+  .dog()
+  .then(response => console.log(`Look at this cute dog! ${response.image}`));
